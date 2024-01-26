14190f1b3ecb034fe48c8090129d5dd1ff413950
GerardCaja <120013160+GerardCaja@users.noreply.github.com>
2024-01-26 11:56:45 +0100

    dsw

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] private Transform userPosition;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+       LoadData();
+
+       userPosition.position = new Vector3 (PlayerPrefs.GetFloat("positionX", 700.3867f), PlayerPrefs.GetFloat("positionY", 77.83562f), PlayerPrefs.GetFloat("positionZ", -128.094f));
+   }
+
+
+   public void SaveData()
+   {
+       PlayerPrefs.SetFloat("positionX", userPosition.position.x);
+       PlayerPrefs.SetFloat("positionY", userPosition.position.y);
+       PlayerPrefs.SetFloat("positionZ", userPosition.position.z);
+   }
+
+   void OnTriggerEnter(Collider collider)
+   {
+        if (collider.gameObject.layer == 7)
+        {
+           SaveData(); 
+        }
+   }
+
+   void LoadData()
+   {
+    PlayerPrefs.GetFloat("positionX", 700.3867f);
+    PlayerPrefs.GetFloat("positionY", 77.83562f);
+    PlayerPrefs.GetFloat("positionZ", -128.094f);
+   }
+}
